9863c10911c4b5747d740b45369746eee70ce1e3
fabrizio-stocco <fabrizio.stocco@fabriziostocco.xyz>
2021-07-11 17:22:25 +0200

    Initial Commit

---
+++ Panorama/PanoramaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PanoramaApp.swift
+//  Panorama
+//
+//  Created by Fabrizio Stocco on 24/11/20.
+//
+
+import SwiftUI
+
+@main
+struct PanoramaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
